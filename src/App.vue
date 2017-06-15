e96a42fd8737ad4580c4c4da6a741e5c3d6aed92
ygx980928 <1041735536@qq.com>
2017-06-15 16:41:07 +0800

    JD 618 home page

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <advertising></advertising>
+    <!--头部广告-->
+   <typerarea></typerarea>
+    <!--版心-->
+  </div>
+</template>
+<script>
+  import advertising from './components/advertising.vue'
+  import typerarea from './components/Typearea.vue'
+  export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+    components:{
+      advertising,
+      typerarea
+    }
+}
+</script>
+
+
---
+++ src/components/Typearea.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="dbx">
+      <div class="navbg"></div>
+      <div class="bx">
+          <nav-a></nav-a>
+      </div>
+
+  </div>
+</template>
+<script>
+    import     navA   from './nav.vue'
+    export default{
+        name:'bx',
+        data(){
+            return{
+
+            }
+        },
+        components:{
+              navA
+        }
+    }
+</script>
+<style>
+    #dbx{
+        width: 100%;
+    }
+    .bx{
+        width: 1190px;
+        margin: 0 auto;
+    }
+    .navbg{
+        width: 100%;
+        height: 30px;
+        background: #ddd;
+        position: absolute;
+        z-index: -1;
+    }
+</style>
